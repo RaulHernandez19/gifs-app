e875c1f0ef4d543a978dd9e0c80d43f0dfb82685
RaulHernandez19 <163434295+RaulHernandez19@users.noreply.github.com>
2024-04-18 09:17:22 -0700

    Finish
    Ready

---
+++ sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+constructor(private gifsService: GifsService){}
+
+get tags(){
+  return this.gifsService.tagsHistory;
+}
+
+   @Output() reutilizableSearch= new EventEmitter<string>();
+
+   searchTag(tag: string): void{
+    this.gifsService.searchTag(tag);
+    }
+
+}
